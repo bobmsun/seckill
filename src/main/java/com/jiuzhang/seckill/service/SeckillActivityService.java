8eaed4be5eb2d2bb0d27a1d2f9ad4f21b4b1bb5f
Shihao Sun <shihao.m.sun@gmail.com>
2021-08-10 14:52:19 -0400

    add payment processing and order query

--- src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
@@ -1,15 +1,19 @@
 package com.jiuzhang.seckill.service;
 
 import com.alibaba.fastjson.JSON;
+import com.jiuzhang.seckill.db.dao.OrderDao;
 import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
 import com.jiuzhang.seckill.db.po.Order;
 import com.jiuzhang.seckill.db.po.SeckillActivity;
 import com.jiuzhang.seckill.service.mq.RocketMQService;
 import com.jiuzhang.seckill.util.SnowFlake;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
+@Slf4j
 @Service
 public class SeckillActivityService {
 
@@ -24,13 +28,30 @@ public class SeckillActivityService {
 
     private SnowFlake snowFlake = new SnowFlake(1, 1);
 
+    @Resource
+    private OrderDao orderDao;
+
+
+    /**
+     * 判断秒杀库存
+     *
+     * @param activityId
+     * @return
+     */
     public boolean seckillStockValidator(long activityId) {
         String key = "stock:" + activityId;
         return service.stockDeductValidation(key);
     }
     // 写完这个 service 层之后，我们需要写一个 controller 控制层，把这个方法给映射出去
 
-
+    /**
+     * 创建订单
+     *
+     * @param seckillActivityId
+     * @param userId
+     * @return
+     * @throws Exception
+     */
     public Order createOrder(long seckillActivityId, long userId) throws Exception {
         /*
          * 1.创建订单
@@ -50,7 +71,32 @@ public class SeckillActivityService {
         // 向消息队列中发送创建订单消息
         rocketMQService.sendMessage("seckill_order", JSON.toJSONString(order));
 
+        /*
+         * 3.发送订单付款状态校验消息
+         * 开源RocketMQ支持延迟消息，但是不支持秒级精度。默认支持18个level的延迟消息，这是通过broker端的messageDelayLevel配置项确定的，如下：
+         * messageDelayLevel=1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+         */
+//        rocketMQService.sendDelayMessage("pay_check",JSON.toJSONString(order),5);
+
         return order;
     }
 
+    /**
+     * 订单支付完成处理
+     * @param orderNo
+     */
+    public void payOrderProcess(String orderNo) {
+        log.info("完成支付订单 订单号：" + orderNo);
+        Order order = orderDao.queryOrder(orderNo);
+        boolean deductStockResult = seckillActivityDao.deductStock(order.getSeckillActivityId());
+
+        if (deductStockResult) {
+            order.setPayTime(new Date());
+            // 0 没有可用库存，无效订单
+            // 1 已创建并等待付款
+            // 2 完成支付
+            order.setOrderStatus(2);
+            orderDao.updateOrder(order);
+        }
+    }
 }
